b8bab02f7a9e5db47bbdeb69b417cc80404528d5
jagannath behera <jagannathbehera3249@gmail.com>
2024-08-11 21:43:17 +0530

    load more functionality added

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,129 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 interface projectListType {
-  title: string;
-  buttonLabel: string;
   redirectUrl: string;
+  title: string;
 }
 
 export default function Home() {
+  const [visibleItem, setVisibleItem] = useState<number>(12);
   const router = useRouter();
   const projectList: projectListType[] = [
     {
+      title: "Counter",
+      redirectUrl: "/counter",
+    },
+    {
       title: "Calculator",
-      buttonLabel: "Visit",
       redirectUrl: "/calculator",
     },
     {
+      title: "Quiz App",
+      redirectUrl: "/quizapp",
+    },
+    {
       title: "Memory Game",
-      buttonLabel: "Visit",
       redirectUrl: "/memorygame",
     },
     {
       title: "Tic Tac Toe",
-      buttonLabel: "Visit",
       redirectUrl: "/tictactoe",
     },
     {
       title: "Drag Drop",
-      buttonLabel: "Visit",
       redirectUrl: "/dragdrop",
     },
     {
       title: "Bar Chart",
-      buttonLabel: "Visit",
       redirectUrl: "/barchart",
     },
     {
       title: "Star Rating",
-      buttonLabel: "Visit",
       redirectUrl: "/rating",
     },
     {
       title: "OtpInput",
-      buttonLabel: "Visit",
       redirectUrl: "/otpinput",
     },
     {
       title: "Infinite Scrolling",
-      buttonLabel: "Visit",
       redirectUrl: "/infinitescrolling",
     },
     {
       title: "Accordion",
-      buttonLabel: "Visit",
       redirectUrl: "/accordion",
     },
     {
       title: "LatestJob",
-      buttonLabel: "Visit",
       redirectUrl: "/latestjob",
     },
     {
       title: "Crypto Converter",
-      buttonLabel: "Visit",
       redirectUrl: "/cryptoconverter",
     },
     {
       title: "Product Filter",
-      buttonLabel: "Visit",
       redirectUrl: "/productfilter",
     },
     {
       title: "Custum Tabs",
-      buttonLabel: "Visit",
       redirectUrl: "/customtabs",
     },
     {
       title: "Add Cart Page",
-      buttonLabel: "Visit",
       redirectUrl: "/addtocart",
     },
     {
+      title: "Shopping List",
+      redirectUrl: "/shoppinglist",
+    },
+    {
       title: "Debounce Suggestion",
-      buttonLabel: "Visit",
       redirectUrl: "/debouncesuggestion",
     },
     {
       title: "Transfer Lists",
-      buttonLabel: "Visit",
       redirectUrl: "/transferlists",
     },
     {
       title: "Pogress Bar",
-      buttonLabel: "Visit",
       redirectUrl: "/pogressbar",
     },
     {
       title: "Image Carosel",
-      buttonLabel: "Visit",
       redirectUrl: "/imagecarosel",
+    },
+    {
+      title: "Pagination",
+      redirectUrl: "/pagination",
+    },
+    {
+      title: "Employ Management System",
+      redirectUrl: "/employdatabase",
+    },
+    {
+      title: "Form validation",
+      redirectUrl: "/formvalidation",
+    },
+    {
+      title: "Morgage Calculator",
+      redirectUrl: "morgagecalculator",
+    },
+    {
+      title: "Stepper App",
+      redirectUrl: "morgagecalculator",
     },
   ];
   const handleNavigate = (url: string) => {
     router.push(url);
   };
+  console.log(visibleItem);
   return (
-    <div className=" h-auto sm:w-[100vw]  flex    flex-col items-center  gap-10 pt-6  px-10 ">
+    <div className=" h-auto sm:w-[100vw]  flex   flex-col items-center  gap-4 pt-6  px-10 ">
       <h1 className="font-bold text-4xl font-serif">Practice Project</h1>
       <div className="flex flex-col justify-center  flex-wrap sm:flex-row gap-6 ">
-        {projectList.map((item, i) => (
+        {projectList.slice(0, visibleItem).map((item, i) => (
           <div className="card   bg-neutral  w-64 shadow-xl" key={i}>
             <div className="card-body gap-4 items-center text-center">
               <h2 className="card-title font-serif text-xl">{item.title}</h2>
@@ -116,7 +132,7 @@ export default function Home() {
                   className="btn   btn-primary"
                   onClick={() => handleNavigate(item.redirectUrl)}
                 >
-                  {item.buttonLabel}
+                  Visit
                 </button>
                 {/* <button className="btn btn-neutral">visit</button> */}
               </div>
@@ -124,6 +140,12 @@ export default function Home() {
           </div>
         ))}
       </div>
+      <button
+        className="btn btn-outline btn-secondary btn-md"
+        onClick={() => setVisibleItem(visibleItem + 4)}
+      >
+        LoadMore
+      </button>
     </div>
   );
 }
